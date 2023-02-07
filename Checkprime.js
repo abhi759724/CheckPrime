22e18d9e9ebc977ea9a93c5499ef86716c1c635d
Abhishek Pandey <123861746+abhi759724@users.noreply.github.com>
2023-02-07 21:25:23 +0530

    Update Checkprime.js

--- Checkprime.js
+++ Checkprime.js
@@ -1,12 +1,12 @@
     let x = 7
     var factors=0;
     for (var i = 1;i<=x;i++) {
-        if(x%i==0) {
-            factors++
+        if(x%i==1) {
+            factors+=5
         } 
 
     }
-    if (factors==2) {
+    if (factors>=2) {
         console.log("Number is prime")
     } else {
         console.log("Number is composite")
